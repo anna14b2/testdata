2a60a1388179989bfa63feba152c5b9319f19892
anna14b2 <anna14b2@stud.kea.dk>
2021-10-04 14:30:25 +0200

    location changed

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const urlParams = new URLSearchParams(window.location.search);
 const spirit = urlParams.get("location");
 console.log(location);
 
-const url = `https://kea2semester-c983.restdb.io/rest/iceland?q={"location" : {"$in" : ["${location}"]}}`;
+const url = `https://kea2semester-c983.restdb.io/rest/iceland? q={"location" : {"$in" : ["${location}"]}}`;
 console.log(url);
 
 const options = {
@@ -10,10 +10,6 @@ const options = {
     "x-apikey": "615322a8dfa7346e2f96912a",
   },
 };
-
-// .catch((err) => {
-//     console.error(err);
-//   });
 
 fetch(url, options)
   .then((response) => {
@@ -26,9 +22,8 @@ fetch(url, options)
     // we have the data
     getIcelandPlaces(data);
   })
-  .catch((e) => {
-    // Something went wrong
-    console.error("An error occured:", e.message);
+  .catch((err) => {
+    console.error(err);
   });
 
 function getIcelandPlaces(place) {
@@ -42,11 +37,13 @@ function showPlace(place) {
   // clone the template
   const copy = template.cloneNode(true);
   // make the place page match the place we are gonna click
-  //   copy.querySelector("a").setAttribute("href", "list.html?iceland=" + iceland.Name);
-  //   console.log(iceland.Name);
+  copy
+    .querySelector("a")
+    .setAttribute("href", "place.html?place=" + iceland.Name);
+  console.log(iceland.Name);
   //change dynamic data
   document.querySelector("h2").textContent = iceland.name;
-  copy.querySelector("img").setAttribute("src", img);
+  copy.querySelector("img").setAttribute("src", iceland.img);
   copy.querySelector("img").setAttribute("alt", iceland.Name);
   //append it to main
   const elemParent = document.querySelector("main");
